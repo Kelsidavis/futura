390809f93a8888f391bffbcf4bd6252216a3d0b7
kelsi davis <kelsihates2fa@gmail.com>
2026-01-21 11:47:39 -0800

    Add comprehensive file mode constants to sys/stat.h
    - Add file type mask S_IFMT and all file type constants (S_IFLNK, S_IFBLK, etc.)
    - Add file type test macros (S_ISDIR, S_ISREG, S_ISCHR, S_ISBLK, etc.)
    - Add special permission bits (S_ISUID, S_ISGID, S_ISVTX)
    - Add complete user/group/other permission bits with rwx masks
    - All constants guarded with #ifndef for compatibility

--- include/sys/stat.h
+++ include/sys/stat.h
@@ -58,27 +58,104 @@ struct stat {
 
 #endif /* !has_include_next */
 
-/* File type macros - always define these */
+/* File type mask and constants */
+#ifndef S_IFMT
+#define S_IFMT   0170000  /* File type mask */
+#endif
 #ifndef S_IFSOCK
-#define S_IFSOCK 0140000
+#define S_IFSOCK 0140000  /* Socket */
+#endif
+#ifndef S_IFLNK
+#define S_IFLNK  0120000  /* Symbolic link */
 #endif
 #ifndef S_IFREG
-#define S_IFREG  0100000
+#define S_IFREG  0100000  /* Regular file */
+#endif
+#ifndef S_IFBLK
+#define S_IFBLK  0060000  /* Block device */
+#endif
+#ifndef S_IFDIR
+#define S_IFDIR  0040000  /* Directory */
 #endif
 #ifndef S_IFCHR
-#define S_IFCHR  0020000
+#define S_IFCHR  0020000  /* Character device */
+#endif
+#ifndef S_IFIFO
+#define S_IFIFO  0010000  /* FIFO (named pipe) */
 #endif
 
-/* File permission macros - always define these */
+/* File type test macros */
+#ifndef S_ISREG
+#define S_ISREG(m)  (((m) & S_IFMT) == S_IFREG)
+#endif
+#ifndef S_ISDIR
+#define S_ISDIR(m)  (((m) & S_IFMT) == S_IFDIR)
+#endif
+#ifndef S_ISCHR
+#define S_ISCHR(m)  (((m) & S_IFMT) == S_IFCHR)
+#endif
+#ifndef S_ISBLK
+#define S_ISBLK(m)  (((m) & S_IFMT) == S_IFBLK)
+#endif
+#ifndef S_ISFIFO
+#define S_ISFIFO(m) (((m) & S_IFMT) == S_IFIFO)
+#endif
+#ifndef S_ISLNK
+#define S_ISLNK(m)  (((m) & S_IFMT) == S_IFLNK)
+#endif
+#ifndef S_ISSOCK
+#define S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)
+#endif
+
+/* Special permission bits */
+#ifndef S_ISUID
+#define S_ISUID  04000    /* Set user ID on execution */
+#endif
+#ifndef S_ISGID
+#define S_ISGID  02000    /* Set group ID on execution */
+#endif
+#ifndef S_ISVTX
+#define S_ISVTX  01000    /* Sticky bit */
+#endif
+
+/* User (owner) permissions */
+#ifndef S_IRWXU
+#define S_IRWXU  0700     /* User rwx mask */
+#endif
 #ifndef S_IRUSR
-#define S_IRUSR 0400
+#define S_IRUSR  0400     /* User read */
 #endif
 #ifndef S_IWUSR
-#define S_IWUSR 0200
+#define S_IWUSR  0200     /* User write */
+#endif
+#ifndef S_IXUSR
+#define S_IXUSR  0100     /* User execute */
+#endif
+
+/* Group permissions */
+#ifndef S_IRWXG
+#define S_IRWXG  0070     /* Group rwx mask */
 #endif
 #ifndef S_IRGRP
-#define S_IRGRP 0040
+#define S_IRGRP  0040     /* Group read */
+#endif
+#ifndef S_IWGRP
+#define S_IWGRP  0020     /* Group write */
+#endif
+#ifndef S_IXGRP
+#define S_IXGRP  0010     /* Group execute */
+#endif
+
+/* Other (world) permissions */
+#ifndef S_IRWXO
+#define S_IRWXO  0007     /* Other rwx mask */
 #endif
 #ifndef S_IROTH
-#define S_IROTH 0004
+#define S_IROTH  0004     /* Other read */
+#endif
+#ifndef S_IWOTH
+#define S_IWOTH  0002     /* Other write */
+#endif
+#ifndef S_IXOTH
+#define S_IXOTH  0001     /* Other execute */
 #endif
